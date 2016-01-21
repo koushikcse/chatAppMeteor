788c6729e53cb4c4a86d4a2222ff5ef6814ad4c1
Soubhik Mondal <soubhik.mondal@innofied.com>
2016-01-21 19:29:15 +0530

    Updated various things, especially UI

---
+++ app/client/compatibility/main.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//     var height = 80 + 54;
+
+//     console.log("1");
+
+//     var win_height = $(window).height();
+
+//     $(".chat-view").css("min-height", (win_height - height) + "px");
+//     console.log("2");
+// });
--- app/client/templates/layouts/chat_page_layout/chat_page_layout.js
+++ app/client/templates/layouts/chat_page_layout/chat_page_layout.js
@@ -1,31 +1,18 @@
 var self_id = "1";
 
+// $(document).ready(function() {
+//     var height = 80 + 54;
+
+//     console.log("1");
+
+//     var win_height = $(window).height();
+
+//     $(".chat-view").css("min-height", (win_height - height) + "px");
+//     console.log("2");
+// });
+
+
 Template.ChatPageLayout.helpers({
-    /*"data": [{
-        "user_id": "1",
-        "msg": "Some message",
-        "msg_time": "12/12/15"
-    }, {
-        "user_id": "2",
-        "msg": "Some message",
-        "msg_time": "12/12/15"
-    }, {
-        "user_id": "1",
-        "msg": "Some message",
-        "msg_time": "12/12/15"
-    }, {
-        "user_id": "2",
-        "msg": "Some message",
-        "msg_time": "12/12/15"
-    }, {
-        "user_id": "2",
-        "msg": "Some message",
-        "msg_time": "12/12/15"
-    }, {
-        "user_id": "1",
-        "msg": "Some message",
-        "msg_time": "12/12/15"
-    }],*/
     data: function() {
         var data = [{
             "user_id": "1",
@@ -71,6 +58,10 @@ Template.ChatPageLayout.events({
 
 Template.ChatPageLayout.onCreated(function() {});
 
-Template.ChatPageLayout.onRendered(function() {});
+Template.ChatPageLayout.onRendered(function() {
+    var height = 80 + 54;
+
+    $(".chat-view").css("max-height", ($(window).height() - height) + "px");
+});
 
 Template.ChatPageLayout.onDestroyed(function() {});
