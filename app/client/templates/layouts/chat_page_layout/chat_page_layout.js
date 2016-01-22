dcb57dde3e78d963f50d2803f08c57f6e4baefd6
Soubhik Mondal <soubhik.mondal@innofied.com>
2016-01-22 11:24:38 +0530

    Added basic styles

--- app/client/templates/layouts/chat_page_layout/chat_page_layout.js
+++ app/client/templates/layouts/chat_page_layout/chat_page_layout.js
@@ -1,44 +1,54 @@
 var self_id = "1";
 
-// $(document).ready(function() {
-//     var height = 80 + 54;
-
-//     console.log("1");
-
-//     var win_height = $(window).height();
-
-//     $(".chat-view").css("min-height", (win_height - height) + "px");
-//     console.log("2");
-// });
-
-
 Template.ChatPageLayout.helpers({
     data: function() {
         var data = [{
-            "user_id": "1",
-            "msg": "Some message",
-            "msg_time": "12/12/15"
-        }, {
-            "user_id": "2",
-            "msg": "Some message",
-            "msg_time": "12/12/15"
-        }, {
-            "user_id": "1",
-            "msg": "Some message",
-            "msg_time": "12/12/15"
-        }, {
-            "user_id": "2",
-            "msg": "Some message",
-            "msg_time": "12/12/15"
-        }, {
-            "user_id": "2",
-            "msg": "Some message",
-            "msg_time": "12/12/15"
-        }, {
-            "user_id": "1",
-            "msg": "Some message",
-            "msg_time": "12/12/15"
-        }];
+                "user_id": "1",
+                "msg": "Some message",
+                "msg_time": "12/12/15 at 15:10"
+            }
+            /*, {
+                        "user_id": "2",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "1",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "2",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "2",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "1",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "2",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "1",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "2",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "2",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }, {
+                        "user_id": "1",
+                        "msg": "Some message",
+                        "msg_time": "12/12/15 at 15:10"
+                    }*/
+        ];
 
         data.forEach(function(elem) {
             if (elem.user_id === self_id) {
@@ -62,6 +72,7 @@ Template.ChatPageLayout.onRendered(function() {
     var height = 80 + 54;
 
     $(".chat-view").css("max-height", ($(window).height() - height) + "px");
+    $(".chat-view").css("height", ($(window).height() - height) + "px");
 });
 
 Template.ChatPageLayout.onDestroyed(function() {});
--- app/client/templates/layouts/user_list_layout/user_list_layout.js
+++ app/client/templates/layouts/user_list_layout/user_list_layout.js
@@ -5,19 +5,19 @@ Template.UserListLayout.helpers({
     "data": [{
         "name": "Soubhik Mondal",
         "last_msg": "Hey there!",
-        "last_msg_time": "12.12.15"
+        "last_msg_time": "12.12.15@15:10"
     }, {
         "name": "Soubhik Mondal",
         "last_msg": "Hey there!",
-        "last_msg_time": "12.12.15"
+        "last_msg_time": "12.12.15@15:10"
     }, {
         "name": "Soubhik Mondal",
         "last_msg": "Hey there!",
-        "last_msg_time": "12.12.15"
+        "last_msg_time": "12.12.15@15:10"
     }, {
         "name": "Soubhik Mondal",
         "last_msg": "Hey there!",
-        "last_msg_time": "12.12.15"
+        "last_msg_time": "12.12.15@15:10"
     }]
 
 });
